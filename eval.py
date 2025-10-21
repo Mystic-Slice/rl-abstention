45ddab7c088b51ffb193c70d859ceab1fa8c3b00
Akanksha Sanjay Mahajan <am59710@usc.edu>
2025-10-20 17:32:41 -0700

    Removed extra backslash

--- eval.py
+++ eval.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 import re
 
 def extract_answer(completion):
-    match = re.search(r"<answer>\s*([A-Da-d])[^<]*<\/answer>", completion)
+    match = re.search(r"<answer>\s*([A-Da-d])[^<]*</answer>", completion)
     if match is not None:
         return match.group(1).strip()
     return None
