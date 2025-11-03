01b8c6029a7e56342c64492d9115b486d60ee379
AkankshaSM <am59710@usc.edu>
2025-11-03 15:50:00 -0800

    batch processing chunked() util added

--- utils.py
+++ utils.py
@@ -1,3 +1,11 @@
 # TODO: remove if not used
 def clamp(value, min_val, max_val):
     return max(min_val, min(value, max_val))
+
+def chunked(iterable, n):
+    it = iter(iterable)
+    while True:
+        batch = list(islice(it, n))
+        if not batch:
+            break
+        yield batch
